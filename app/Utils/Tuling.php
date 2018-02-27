fa9da91a93f0390fee6b1450d0b83f54ffd2017e
questionyu <questionyu@outlook.com>
2018-02-27 10:50:41 +0800

    Fix tuling bot message.

--- app/Utils/Tuling.php
+++ app/Utils/Tuling.php
@@ -10,6 +10,9 @@ class Tuling
     public static function chat($user, $text)
     {
         if (Config::get('enable_tuling') == 'true') {
+            if (substr($text, 0, 1) == '/') {
+                $text = substr($text, 1);
+            }
             $data = array();
             $data['key'] = Config::get('tuling_apikey');
             $data['userid'] = $user;
@@ -26,7 +29,33 @@ class Tuling
             
             $result = $sock->fetch_body();
             $result_array = json_decode($result, true);
-            return $result_array['text'];
+
+            $result_content = $result_array['text'];
+            //文本类
+            if ($result_array['code'] == '100000') {
+                //Do nothing
+            }
+            //链接类
+            elseif ($result_array['code'] == '200000') {
+                $result_content = $result_content.PHP_EOL.
+                '[打开页面]('.$result_array['url'].')';
+            }
+            //新闻类
+            elseif ($result_array['code'] == '302000') {
+                foreach ($result_array['list'] as $key) {
+                    $result_content = $result_content.PHP_EOL.PHP_EOL.
+                    '['.$key['article'].']('.$key['detailurl'].')';
+                }
+            }
+            //菜谱类
+            elseif ($result_array['code'] == '308000') {
+                foreach ($result_array['list'] as $key) {
+                    $result_content = $result_content.PHP_EOL.PHP_EOL.
+                    '['.$key['name'].' '.$key['info'].']('.$key['detailurl'].')';
+                }
+            }
+
+            return $result_content;
         }
     }
 }
